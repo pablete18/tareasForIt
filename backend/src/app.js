4838ce902c4d952d85eb786efe8097c73e30a39e
pablete18 <pabloealcaraz@gmail.com>
2025-03-22 10:27:44 -0300

    [fix]borrando variables sin utilizar

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,5 @@
 import express from 'express'
 import router from './routes/taskRoutes.js'
-import morgan from 'morgan'
-import cors from 'cors'
-import path from 'path'
-
 
 const app = express()
 
