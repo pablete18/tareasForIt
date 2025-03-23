e685b5c841c3dea105bbc098703400f0ca4ed62f
pablete18 <pabloealcaraz@gmail.com>
2025-03-23 15:44:44 -0300

    [fix install cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routes/taskRoutes.js'
 
 const app = express()
 
 const PORT = 3000
 
+app.use(cors())
 app.use(express.json());
 
 app.use(express.urlencoded({extended : false}))
